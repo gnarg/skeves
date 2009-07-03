a27c00dab248bd409ae8afb28a1dd99d64b8f98c
Jon Guymon <guymon@slackworks.com>
2009-07-03 17:43:31 -0400

    initial import

---
+++ skeves.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'sinatra'
+require 'reve'
+require 'yaml'
+require 'lib/skills'
+
+get '/' do
+  eve_auth = YAML.load(File.open(File.dirname(__FILE__) + '/eve_auth.yml'))
+  
+  api = Reve::API.new(eve_auth['user_id'], eve_auth['api_key'])
+  queue = []
+  api.characters.each do |character|
+    queue = api.skill_queue(:characterid => character.id)
+    break if queue.any?
+  end
+  
+  return 'No skills in queue.' if queue.empty?
+  
+  @skills = queue.map{|s| Skeves::Skill.new(s)}
+
+  erb :index
+end

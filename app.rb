77096aef39b19821dc1ea4f9a0dd7741d144b306
Jon Guymon <guymon@slackworks.com>
2009-07-09 19:02:01 -0400

    remogrifying the app to deploy to google app engine

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'reve'
+require 'yaml'
+require 'lib/skills'
+
+get '/' do
+  eve_auth = YAML.load(File.open(options.root + '/config/eve_auth.yml'))
+  
+  api = Reve::API.new(eve_auth['user_id'], eve_auth['api_key'])
+  queue = []
+  api.characters.each do |character|
+    queue = api.skill_queue(:characterid => character.id)
+    if queue.any?
+      @character = character
+      break
+    end
+  end
+  
+  return 'No skills in queue.' if queue.empty?
+  
+  @skills = queue.map{|s| Skeves::Skill.new(s)}
+
+  erb :index
+end
--- config.ru
+++ config.ru
@@ -1,4 +1,7 @@
-require 'skeves'
+require 'rubygems'
+require 'appengine-apis/urlfetch'
+Net::HTTP = AppEngine::URLFetch::HTTP
+require 'app'
 
 set :run, false
 set :environment, :production

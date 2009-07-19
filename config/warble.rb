769083fccfea52f8530d50c267ec7c1b4a50ff18
Nic Benders <nic.benders@gmail.com>
2009-07-18 22:16:29 -0700

    Get datastore-indexes.xml into the WEB-INF

--- config/warble.rb
+++ config/warble.rb
@@ -15,6 +15,7 @@ Warbler::Config.new do |config|
   # Additional files/directories to include, above those in config.dirs
   # config.includes = FileList["db"]
   config.includes = FileList["appengine-web.xml", "app.rb", "models.rb", "config.ru", "cron.xml"]
+  config.includes += FileList["datastore-indexes.xml"]
   # Additional files/directories to exclude
   # config.excludes = FileList["lib/tasks/*"]
  
